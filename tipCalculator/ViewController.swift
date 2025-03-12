cc077eb2a715a05d56dcc4bbdcab3da71ff6aac9
Avb2 <bringuel.alexander@gmail.com>
2025-03-11 22:47:36 -0400

    Initial Commit

---
+++ tipCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tipCalculator
+//
+//  Created by Alex Bringuel on 3/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
