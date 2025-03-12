ee0309eea865310c94196d129d8f9d72d34ae461
Avb2 <bringuel.alexander@gmail.com>
2025-03-11 23:16:41 -0400

    Added button logic

--- tipCalculator/ViewController.swift
+++ tipCalculator/ViewController.swift
@@ -9,15 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var inputTxt: UITextField!
+    @IBOutlet weak var outputLabel: UILabel!
     
-    @IBOutlet weak var eighteenPercentBtn: UIButton!
-    @IBOutlet weak var fifteenPercentBtn: UIButton!
+    @IBAction func fifteenPBtn(_ sender: UIButton) {
+        let input = Double(inputTxt.text ?? "0")
+        
+        let total = (input ?? 0) * 0.15 + (input ?? 0)
+        
+        outputLabel.text = "Subtotal: $\(input ?? 0)\nTotal: $\(total)"
+    }
     
-    @IBOutlet weak var twentyPercentBtn: UIButton!
+    @IBAction func eighteenPBtn(_ sender: UIButton) {
+        let input = Double(inputTxt.text ?? "0")
+        
+        let total = (input ?? 0) * 0.18 + (input ?? 0)
+        
+        outputLabel.text = "Subtotal: $\(input ?? 0)\nTotal: $\(total)"
+    }
+    
+    @IBAction func twentyPBtn(_ sender: UIButton) {
+        let input = Double(inputTxt.text ?? "0")
+        
+        let total = (input ?? 0) * 0.2 + (input ?? 0)
+        
+        outputLabel.text = "Subtotal: $\(input ?? 0)\nTotal: $\(total)"
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let tap: UIGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
+        
+        view.addGestureRecognizer(tap)
+    }
+    
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
     }
     
     
