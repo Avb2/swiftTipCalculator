d7818e18471f557c347687097ec1b04671fc7f83
Avb2 <bringuel.alexander@gmail.com>
2025-03-11 23:00:03 -0400

    first commit

--- tipCalculator/ViewController.swift
+++ tipCalculator/ViewController.swift
@@ -8,11 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var inputTxt: UITextField!
+    
+    @IBOutlet weak var eighteenPercentBtn: UIButton!
+    @IBOutlet weak var fifteenPercentBtn: UIButton!
+    
+    @IBOutlet weak var twentyPercentBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
 
 
 }
